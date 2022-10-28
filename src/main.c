e9379905eda73a03f701134b7257997931e944c1
Abhijit0303 <73029055+Abhijit0303@users.noreply.github.com>
2022-10-28 08:56:11 +0530

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,16 +1,16 @@
-#include "struct.h"
-#include "evaluate.c"
+#include "qes.h"
+
 
 
 int main(int argc, char *argv[]){
 	if(argc == 3) {
 		FILE *questionPaper = fopen(argv[1], "r");
 
-    	FILE *answerSheet = fopen(argv[2], "r");
+    		FILE *answerSheet = fopen(argv[2], "r");
     
-    	evaluate(questionPaper,answerSheet);
+    		evaluate(questionPaper,answerSheet);
 
-    	return 0;
+    		return 0;
 	}
 
 	return 1;    
